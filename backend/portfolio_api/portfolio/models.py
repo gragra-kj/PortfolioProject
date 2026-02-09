a79fe2d989a94b8082d7e20720369384cf79969b
gragra-kj <muthuigracia27@gmail.com>
2026-02-09 09:45:48 +0300

    backend

--- backend/portfolio_api/portfolio/models.py
+++ backend/portfolio_api/portfolio/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 # Create your models here.
 
+class Skill(models.Model):
+    name=models.CharField(max_length=100)
+    level=models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+    
+class Project(models.Model):
+    title=models.models.CharField(max_length=500)
+    description=models.TextField()
+    tech=models.CharField(max_length=250)
+    github_urs=models.URLField()
+    live_urls=models.URLField(blank=True)
+    image=models.URLField()
+    
+    def __str__(self):
+        return self.title
+    
+    
